1c7669229e6e08aeb8392ef51f393dd5de54e372
Luis Carvajal <luis.carvajalrojas@ucr.ac.cr>
2020-12-14 02:49:09 -0600

    cosas

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,15 +1,59 @@
-console.log('Hola');
 
-const nombre = 'Luis';
-const apellido = 'Carvajal';
+// Desestructuración
 
-const nombreCompleto = `${nombre} ${apellido}`;
+const persona = {
+    nombre: 'Tony',
+    edad: 45,
+    clave: 'Ironman'
+};
 
-console.log(nombreCompleto);
+const {nombre:nombre2, edad, clave} = persona;
+
+console.log(nombre2);
+console.log(persona.edad);
+console.log(persona.clave);
 
 
-function getSaludo(){
-    return 'Hola';
-}
 
-console.log(`Este es un texto ${getSaludo()}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const persona = {
+    nombre: 'Tony',
+    apellido: 'Stark',
+};
+
+const persona2 = {...persona};
+persona2.nombre = 'Peter';
+console.log(persona.nombre);
+console.log(persona2.nombre);*/
+
+//const arreglo = new Array( 100);
+// const arr = [];
+// arr.push(1);
+// arr.push(2);
+// arr.push(3);
+// arr.push(4);
+// arr.push(5);
+
+// const arr2 = arr.map(function(x){
+//     return x * 2;
+// });
+
+// console.log(arr);
+// console.log(arr2);
