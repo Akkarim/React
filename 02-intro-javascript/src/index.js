8fe36dbb5193dae1f641b7f3ec466f6379c604bd
Luis Carvajal <luis.carvajalrojas@ucr.ac.cr>
2020-12-14 01:19:03 -0600

    intro a js

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,15 @@
+console.log('Hola');
+
+const nombre = 'Luis';
+const apellido = 'Carvajal';
+
+const nombreCompleto = `${nombre} ${apellido}`;
+
+console.log(nombreCompleto);
+
+
+function getSaludo(){
+    return 'Hola';
+}
+
+console.log(`Este es un texto ${getSaludo()}`);
