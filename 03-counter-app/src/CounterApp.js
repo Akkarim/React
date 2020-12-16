77074ddf2991b92200baab27485fffb76c4fe9a6
Luis Carvajal <luisda7cr@gmail.com>
2020-12-15 23:48:53 -0600

    keep it

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,14 @@
+import propTypes 
+
+const CounterApp = ({value}) => {
+    return (
+        <>
+        <h1>Counter App</h1>
+        <h2>{ value }</h2>
+        </>
+    );
+}
+
+CounterApp.propTypes
+
+export default CounterApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
 import './index.css';
+import CounterApp from './CounterApp';
 
 const divRoot = document.querySelector('#root');
-
-ReactDOM.render(<PrimeraApp saludo='Hola, Soy Goku!' />, divRoot);
+ReactDOM.render(<CounterApp value={77} />, divRoot);
+// ReactDOM.render(<PrimeraApp saludo='Hola, Soy Goku!' />, divRoot);
