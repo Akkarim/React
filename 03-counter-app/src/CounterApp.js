13ea22cb5a186209df536ca235b8d89ebe86ae1e
Luis Carvajal <luis.carvajalrojas@ucr.ac.cr>
2020-12-16 01:40:42 -0600

    final de seccion 3

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,14 +1,37 @@
-import propTypes 
+import PropTypes from 'prop-types' 
+import React, {useState} from 'react'
 
 const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState(0);
+
+
+
+    const handleAdd = (e) => {
+        setCounter(counter + 1);
+    }
+    const handleSubs = (e) => {
+        setCounter(counter - 1);
+    }
+    const reset = (e) => {
+        setCounter(value);
+    }
+    
+
     return (
         <>
         <h1>Counter App</h1>
         <h2>{ value }</h2>
+        
+        <button onClick={ handleAdd }>+1</button>
+        <button onClick={ reset }>Reset</button>
+        <button onClick={ handleSubs }>-1</button>
         </>
     );
 }
 
-CounterApp.propTypes
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
 
 export default CounterApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+//import PrimeraApp from './PrimeraApp';
 import './index.css';
 import CounterApp from './CounterApp';
 
 const divRoot = document.querySelector('#root');
-ReactDOM.render(<CounterApp value={77} />, divRoot);
+ReactDOM.render(<CounterApp value={5} />, divRoot);
 // ReactDOM.render(<PrimeraApp saludo='Hola, Soy Goku!' />, divRoot);
